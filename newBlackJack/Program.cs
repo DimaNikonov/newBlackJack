9248a8a92dd2bef845dd959200ca13535c8c3bab
Валентина <@VAL>
2017-04-18 13:10:02 +0300

    correct

--- newBlackJack/Program.cs
+++ newBlackJack/Program.cs
@@ -4,323 +4,314 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace newBlackJack
+namespace NewBlackJack
 {
-
-
     class Program
     {
-        static int getMoreCardsC(List<card> player, ref int index, card[] cards)
+        static int GetMoreCardsC(List<Card> Player, ref int Index, Card[] Cards)
         {
             int sumC;
             Console.WriteLine("computer make choice.");
             do
             {
                 sumC = 0;
-                for (int i = 0; i < player.Count; i++)
+                for (int i = 0; i < Player.Count; i++)
                 {
-                    sumC += (int)player[i].znachenie;
+                    sumC += (int)Player[i].Face;
                 }
                 if (sumC >= 16) break;
-                player.Add(cards[index]);
-                index++;
-                printCardsC(player);
+                Player.Add(Cards[Index]);
+                Index++;
+                PrintCardsC(Player);
             } while (true);
             return sumC;
         }
-        static int getMoreCardsP(List<card> player, ref int index, card[] cards)
+
+        static int GetMoreCardsP(List<Card> Player, ref int Index, Card[] Cards)
         {
             int sumP;
             Console.WriteLine("player1 make choice.");
             do
             {
                 sumP = 0;
-                foreach (card item in player)
+                foreach (Card item in Player)
                 {
-                    sumP += (int)item.znachenie;
+                    sumP += (int)item.Face;
                 }
-
                 Console.WriteLine("get more card? if yes put 'y', if no - 'n' ");
                 var choice = char.Parse(Console.ReadLine());
                 if (choice == 'n') break;
-                player.Add(cards[index]);
-                index++;
-                printCardsP(player);
+                Player.Add(Cards[Index]);
+                Index++;
+                PrintCardsP(Player);
             } while (true);
             return sumP;
         }
-        static void printCardsC(List<card> player)
+
+        static void PrintCardsC(List<Card> Player)
         {
             Console.WriteLine("cards computer:");
-            print(player);
+            Print(Player);
         }
-        static void printCardsP(List<card> player)
+
+        static void PrintCardsP(List<Card> player)
         {
             Console.WriteLine("cards player1:");
-            print(player);
+            Print(player);
         }
-        static int ktoPerviy()
+
+        static int WhoFirst()
         {
             Console.WriteLine("pres 0 if first go player1, pres 1 if first go computer ");
-            int perviy = int.Parse(Console.ReadLine());
-            return perviy;
+            int First = int.Parse(Console.ReadLine());
+            return First;
         }
-        static void mixCards(card[] cards)
+
+        static void MixCards(Card[] Cards)
         {
             Random rnd = new Random();
-            card temp;
+            Card temp;
             int rndIndex = 0;
-            for (int i = 0; i < cards.Length; i++)
+            for (int i = 0; i < Cards.Length; i++)
             {
-                rndIndex = rnd.Next(0, cards.Length);
-                temp = cards[i];
-                cards[i] = cards[rndIndex];
-                cards[rndIndex] = temp;
+                rndIndex = rnd.Next(0, Cards.Length);
+                temp = Cards[i];
+                Cards[i] = Cards[rndIndex];
+                Cards[rndIndex] = temp;
             }
         }
-        static void fillCards(card[] cards)
+
+        static void FillCards(Card[] Cards)
         {
+            const int QuantitySuite = 4;
+            const int QuantityFace = 11;
+            const int ValueFirstCard = 2;
+            const int OutCardValue = 5;
             int index = 0;
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < QuantitySuite; i++)
             {
-                for (int j = 2; j <= 11; j++)
+                for (int j = ValueFirstCard; j <= QuantityFace; j++)
                 {
-                    if (j == 5) continue;
+                    if (j == OutCardValue) continue;
                     else
                     {
-                        cards[index] = new card()
+                        Cards[index] = new Card()
                         {
-                            mast = (mast)i,
-                            znachenie = (znachenie)j
+                            Suite = (Suite)i,
+                            Face = (Face)j
                         };
                         index++;
                     }
                 }
             }
         }
-        static void print( List<card> player)
+
+        static void Print(List<Card> Player)
         {
-            foreach (card item in player)
+            foreach (Card item in Player)
             {
-                Console.WriteLine("{0,6} {1,7}", item.znachenie, item.mast);
+                Console.WriteLine("{0,6} {1,7}", item.Face, item.Suite);
             }
         }
 
-        static void print(card[] cards)
+        static void Print(Card[] Cards)
         {
-            for (int i = 0; i < cards.Length; i++)
+            for (int i = 0; i < Cards.Length; i++)
             {
-                Console.WriteLine("{0,6} {1,7}", cards[i].znachenie, cards[i].mast);
+                Console.WriteLine("{0,6} {1,7}", Cards[i].Face, Cards[i].Suite);
             }
         }
-        struct card
+
+        static List<Card> PlayerGetFirstCards(List<Card> Player, Card[] Cards)
         {
-            public znachenie znachenie;
-            public mast mast;
+            Player = new List<Card>
+                        {
+                            Cards[0],
+                            Cards[1]
+                        };
+            return Player;
         }
-        enum znachenie
+
+        static List<Card> PlayerGetsecondCards(List<Card> Player, Card[] Cards)
         {
-            valet = 2,
-            dama = 3,
-            korol = 4,
-            six = 6,
-            seven = 7,
-            eight = 8,
-            nine = 9,
-            ten = 10,
-            tuz = 11
+            Player = new List<Card>
+                        {
+                            Cards[2],
+                            Cards[3]
+                        };
+            return Player;
         }
-        enum mast
+
+        static bool EqualAce(List<Card> Player)
         {
-            pika,
-            trefa,
-            bubna,
-            cherva
+            bool equalAce;
+            if (Player[0].Face == Face.Ace && Player[1].Face == Face.Ace) equalAce = true;
+            else equalAce = false;
+            return equalAce;
+        }
+
+        static bool NotEqualAce(List<Card> Player)
+        {
+            bool equalAce;
+            if (Player[0].Face != Face.Ace && Player[1].Face != Face.Ace) equalAce = true;
+            else equalAce = false;
+            return equalAce;
+        }
+
+        static int CountWinP(int[] CountWin)
+        {
+            Console.WriteLine("player1 win");
+            CountWin[0]++;
+            return CountWin[0];
+        }
+
+        static int CountWinC(int[] CountWin)
+        {
+            Console.WriteLine("computer win");
+            CountWin[1]++;
+            return CountWin[1];
+        }
+
+        static int[] Scoring(int[] CountWin, int SumP, int SumC)
+        {
+            if (SumP > 21 && SumC <= 21)
+            {
+                CountWinC(CountWin);
+            }
+            if (SumP <= 21 && SumC > 21)
+            {
+                CountWinP(CountWin);
+            }
+            if (SumC > 21 && SumP > 21 && SumP > SumC)
+            {
+                CountWinC(CountWin);
+            }
+            if (SumC > 21 && SumP > 21 && SumP < SumC)
+            {
+                CountWinP(CountWin);
+            }
+            if (SumC < 21 && SumP < 21 && SumC > SumP)
+            {
+                CountWinC(CountWin);
+            }
+            if (SumC < 21 && SumP < 21 && SumC < SumP)
+            {
+                CountWinP(CountWin);
+            }
+            if (SumP == SumC)
+            {
+                Console.WriteLine("Draw");
+            }
+            return CountWin;
+        }
+
+        struct Card
+        {
+            public Face Face;
+            public Suite Suite;
+        }
+
+        enum Face
+        {
+            Jack = 2,
+            Queen = 3,
+            King = 4,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Ace = 11
+        }
+
+        enum Suite
+        {
+            Spade,
+            Club,
+            Diamond,
+            Heart
         }
 
         static void Main(string[] args)
         {
-            int countWinP = 0;
-            int countWinC = 0;
-            card[] cards = new card[36];
-            fillCards(cards);
-            print(cards);
+            int[] countWin = new int[2];
+            Card[] Cards = new Card[36];
+            FillCards(Cards);
+            Print(Cards);
             Console.WriteLine();
             do
             {
-                mixCards(cards);
-                print(cards);
+                MixCards(Cards);
+                Print(Cards);
                 Console.WriteLine();
-                int goFirst = ktoPerviy();
-
-                List<card> player1;
-                List<card> computer;
+                int goFirst = WhoFirst();
+                List<Card> player1 = null;
+                List<Card> computer = null;
                 int index = 4;
                 int sumP = 0;
                 int sumC = 0;
                 switch (goFirst)
                 {
                     case 0:
-                        player1 = new List<card>
+                        player1 = PlayerGetFirstCards(player1, Cards);
+                        computer = PlayerGetsecondCards(computer, Cards);
+                        if (EqualAce(player1) && EqualAce(computer))
                         {
-                            cards[0],
-                            cards[1]
-                        };
-                        computer = new List<card>
-                        {
-                            cards[2],
-                            cards[3]
-                        };
-
-                        if (player1[0].znachenie == znachenie.tuz && player1[1].znachenie == znachenie.tuz
-                            && computer[0].znachenie == znachenie.tuz && computer[1].znachenie == znachenie.tuz)
-                        {
-                            Console.WriteLine("nichya");
+                            Console.WriteLine("Draw");
                             continue;
                         }
-                        else if (player1[0].znachenie == znachenie.tuz && player1[1].znachenie == znachenie.tuz
-                            && computer[0].znachenie != znachenie.tuz && computer[1].znachenie != znachenie.tuz)
+                        else if (EqualAce(player1) && NotEqualAce(computer))
                         {
-                            printCardsP(player1);
-                            Console.WriteLine("Player1 win");
-                            countWinP++;
+                            PrintCardsP(player1);
+                            CountWinP(countWin);
                             continue;
                         }
-                        else if (player1[0].znachenie != znachenie.tuz && player1[1].znachenie != znachenie.tuz
-                            && computer[0].znachenie == znachenie.tuz && computer[1].znachenie == znachenie.tuz)
+                        else if (NotEqualAce(player1) && EqualAce(computer))
                         {
-                            printCardsC(computer);
-                            Console.WriteLine("computer win");
-                            countWinC++;
+                            PrintCardsC(computer);
+                            CountWinC(countWin);
                             continue;
                         }
-                        printCardsP(player1);
-                        sumP = getMoreCardsP(player1, ref index, cards);
-
-                        printCardsC(computer);
-                        sumC = getMoreCardsC(computer, ref index, cards);
-
-                        if (sumP > 21 && sumC <= 21)
-                        {
-                            Console.WriteLine("computer win");
-                            countWinC++;
-                        }
-                        if (sumP <= 21 && sumC > 21)
-                        {
-                            Console.WriteLine("player1 win");
-                            countWinP++;
-                        }
-                        if (sumC > 21 && sumP > 21 && sumP > sumC)
-                        {
-                            Console.WriteLine("computer win");
-                            countWinC++;
-                        }
-                        if (sumC > 21 && sumP > 21 && sumP < sumC)
-                        {
-                            Console.WriteLine("player1 win");
-                            countWinP++;
-                        }
-                        if (sumC < 21 && sumP < 21 && sumC > sumP)
-                        {
-                            Console.WriteLine("computer win");
-                            countWinC++;
-                        }
-                        if (sumC < 21 && sumP < 21 && sumC < sumP)
-                        {
-                            Console.WriteLine("player1 win");
-                            countWinP++;
-                        }
-                        if (sumP == sumC)
-                        {
-                            Console.WriteLine("nichya");
-                        }
+                        PrintCardsP(player1);
+                        sumP = GetMoreCardsP(player1, ref index, Cards);
+                        PrintCardsC(computer);
+                        sumC = GetMoreCardsC(computer, ref index, Cards);
+                        Scoring(countWin, sumP, sumC);
                         break;
-
                     case 1:
-                        computer = new List<card>
+                        computer = PlayerGetFirstCards(computer, Cards);
+                        player1 = PlayerGetsecondCards(player1, Cards);
+                        if (EqualAce(player1) && EqualAce(computer))
                         {
-                            cards[0],
-                            cards[1]
-                        };
-                        player1 = new List<card>
-                        {
-                            cards[2],
-                            cards[3]
-                        };
-
-                        if (player1[0].znachenie == znachenie.tuz && player1[1].znachenie == znachenie.tuz
-                            && computer[0].znachenie == znachenie.tuz && computer[1].znachenie == znachenie.tuz)
-                        {
-                            Console.WriteLine("nichya");
+                            Console.WriteLine("Draw");
                             continue;
                         }
-                        else if (player1[0].znachenie == znachenie.tuz && player1[1].znachenie == znachenie.tuz
-                            && computer[0].znachenie != znachenie.tuz && computer[1].znachenie != znachenie.tuz)
+                        else if (EqualAce(player1) && NotEqualAce(computer))
                         {
-                            printCardsP(player1);
-                            Console.WriteLine("Player1 win");
-                            countWinP++;
+                            PrintCardsP(player1);
+                            CountWinP(countWin);
                             continue;
                         }
-                        else if (player1[0].znachenie != znachenie.tuz && player1[1].znachenie != znachenie.tuz
-                            && computer[0].znachenie == znachenie.tuz && computer[1].znachenie == znachenie.tuz)
+                        else if (NotEqualAce(player1) && EqualAce(computer))
                         {
-                            printCardsC(computer);
-                            Console.WriteLine("computer win");
-                            countWinC++;
+                            PrintCardsC(computer);
+                            CountWinC(countWin);
                             continue;
                         }
-
-                        printCardsC(computer);
-                        sumC = getMoreCardsC(computer, ref index, cards);
-
-                        printCardsP(player1);
-                        sumP = getMoreCardsP(player1, ref index, cards);
-
-                        if (sumP > 21 && sumC <= 21)
-                        {
-                            Console.WriteLine("computer win");
-                            countWinC++;
-                        }
-                        if (sumP <= 21 && sumC > 21)
-                        {
-                            Console.WriteLine("player1 win");
-                            countWinP++;
-                        }
-                        if (sumC > 21 && sumP > 21 && sumP > sumC)
-                        {
-                            Console.WriteLine("computer win");
-                            countWinC++;
-                        }
-                        if (sumC > 21 && sumP > 21 && sumP < sumC)
-                        {
-                            Console.WriteLine("player1 win");
-                            countWinP++;
-                        }
-                        if (sumC < 21 && sumP < 21 && sumC > sumP)
-                        {
-                            Console.WriteLine("computer win");
-                            countWinC++;
-                        }
-                        if (sumC < 21 && sumP < 21 && sumC < sumP)
-                        {
-                            Console.WriteLine("player1 win");
-                            countWinP++;
-                        }
-                        if (sumP == sumC)
-                        {
-                            Console.WriteLine("nichya");
-                        }
+                        PrintCardsC(computer);
+                        sumC = GetMoreCardsC(computer, ref index, Cards);
+                        PrintCardsP(player1);
+                        sumP = GetMoreCardsP(player1, ref index, Cards);
+                        Scoring(countWin, sumP, sumC);
                         break;
                 }
                 Console.ReadLine();
-           
                 Console.Clear();
                 Console.WriteLine("do you won to continue? pres 'y' if yes, 'n' - no ");
                 char choice = char.Parse(Console.ReadLine());
                 if (choice == 'n') break;
             } while (true);
-            Console.WriteLine($"plaer1 win:{countWinP}, computer win:{countWinC}");
+            Console.WriteLine($"player1 win:{countWin[0]}, computer win:{countWin[1]}");
             Console.ReadLine();
         }
     }
